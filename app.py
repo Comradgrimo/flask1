ac0476b68d7b297763695698bfa1b4a9dd53f89f
Comradgrimo <comradgrimo@gmail.com>
2019-02-27 20:43:27 +0300

    Добавленны месяца

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask, render_template, send_file, abort, send_from_directory
+from flask import Flask, render_template, send_file, abort, send_from_directory,g
 app = Flask(__name__)
 app.debug = True
 
@@ -10,20 +10,33 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', type="image/x-icon")
 
 @app.route('/')
-@app.route('/<path:path>')
-def dir_viewer(path='./html'):
-    return render_template("hello.html", entries=list(os.scandir(path))).encode('UTF-8')
+def hello():
+    return render_template("hello.html")
 
 @app.route('/graph')
-def dir_viewer1(path='./graph'):
+def graph(path='./graph'):
     return render_template("graph.html", entries=list(os.scandir(path))).encode('UTF-8')
 
 @app.route("/download/<path:path>")
 def download(path):
     return send_file(path)
 
+# @app.route('/january')
+# def january(path='./html/2019/january'):
+#     return render_template("january.html", entries=list(os.scandir(path))).encode('UTF-8')
 
+@app.route('/<month>')
+def year(month):
+    month1 = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+             'november', 'december']
+    if month in month1:
+        return render_template("january.html", entries=list(os.scandir("./html/2019/"+ month)), name_month = month)
+    else:
+        return render_template('404.html')
 
+@app.errorhandler(404)
+def error500(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(host='192.168.15.164', port='5010')
